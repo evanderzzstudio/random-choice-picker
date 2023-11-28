e9a6c682c8223704fd112fbac0db963d0b83d0c8
evanderzzstudio <evanderstudio@gmail.com>
2023-11-28 11:12:52 +0700

    This commit updates the handling of tag input and random selection functionality, while also removing the box-shadow from an element.

--- script.js
+++ script.js
@@ -5,9 +5,41 @@ textarea.focus()
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value)
+
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.valu = ""
+        }, 10)
+        randomSelect()
+    }
 })
 
 function createTags(input) {
-    const tags = input.split(",")
-    console.log(object)
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+
+}
+
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+    }, 100);
+
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag")
+    return tags[]
 }
