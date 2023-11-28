ec05d95b3c9b2fe5923c7f0ecd698b0c6095e777
evanderzzstudio <evanderstudio@gmail.com>
2023-11-28 11:50:07 +0700

    - Refactor code for adding and highlighting tags.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const tagsEl = document.getElementById("tags")
-const textarea = document.getElementById("textarea")
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
 textarea.focus()
 
-textarea.addEventListener("keyup", (e) => {
+textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
         setTimeout(() => {
-            e.target.valu = ""
+            e.target.value = ''
         }, 10)
+
         randomSelect()
     }
 })
 
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-
 
     tagsEl.innerHTML = ''
 
@@ -26,20 +26,44 @@ function createTags(input) {
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
-
 }
-
 
 function randomSelect() {
     const times = 30
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
+
+        if (randomTag !== undefined) {
+            highlightTag(randomTag)
+
+            setTimeout(() => {
+                unHighlightTag(randomTag)
+            }, 100)
+        }
     }, 100);
 
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
 }
 
 function pickRandomTag() {
-    const tags = document.querySelectorAll(".tag")
-    return tags[]
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
